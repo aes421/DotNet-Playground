fad61f087a04bfbccf14e5b8122ca52eb4161d02
Ashley Stallings <aesdev421@gmail.com>
2018-08-27 11:45:32 -0500

    Add project files.

---
+++ LearningDotNet/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LearningDotNet.Startup))]
+namespace LearningDotNet
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
