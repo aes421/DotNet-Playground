635e93897d2a78795036f23edd7a55bdb2c1beeb
Ashley Stallings <aesdev421@gmail.com>
2018-09-06 14:27:07 -0500

    RabbitMQ pub sub example

---
+++ Tutorial3_Publish/Publish.cs
@@ -0,0 +1,49 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace Tutorial3_Publish
+{
+    class Publish
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Publisher online!");
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    //RabbitMQ Messaging Model:
+                    //Producer never interacts with queue.  It interacts with
+                    //exchange.  Exchange handles accepting messages from producer
+                    //and pushing it to the correct queues (multiples, just one, etc.)
+
+                    //options are direct, topic, headers, fanout
+                    channel.ExchangeDeclare("logs", "fanout");
+
+                    string message = GetMessage(args);
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish(
+                         exchange: "logs",
+                         routingKey: "",
+                         basicProperties: null,
+                         body: body
+                    );
+
+                    Console.WriteLine(" [x] Sent {0}", message);
+                }
+
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
+        }
+
+        private static string GetMessage(string[] args)
+        {
+            return ((args.Length > 0) ? string.Join(" ", args) : "Hello World!");
+        }
+    }
+}
---
+++ Tutorial3_Subscribe/Subscribe.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Tutorial3_Subscribe
+{
+    class Subscribe
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Subscriber Online!");
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.ExchangeDeclare(exchange: "logs", type: "fanout");
+
+                    //each subscriber should have a randomly genereated queue name
+                    var queueName = channel.QueueDeclare().QueueName;
+
+                    //bind the queue so the exchange knows about all the subscribers
+                    channel.QueueBind(
+                        queue: queueName,
+                        exchange: "logs",
+                        routingKey: ""
+                        );
+
+                    Console.WriteLine(" [*] Waiting for logs.");
+
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += (model, ea) =>
+                    {
+                        var body = ea.Body;
+                        var message = Encoding.UTF8.GetString(body);
+                        Console.WriteLine(" [x] {0}", message);
+                    };
+                    channel.BasicConsume(queue: queueName,
+                                         autoAck: true,
+                                         consumer: consumer);
+
+                    Console.WriteLine(" Press [enter] to exit.");
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
