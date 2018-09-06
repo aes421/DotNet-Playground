48e09ddc1a359253b0cb4234374b9d52691bed2f
Ashley Stallings <aesdev421@gmail.com>
2018-09-06 12:05:36 -0500

    Playing around with RabbitMQ

---
+++ Receive/Receive.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Receive
+{
+    class Receive
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Ready to receive!");
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    //durable true will ensure we don't lose messages even if the RabitMQ server dies
+                    channel.QueueDeclare(queue: "task_queue",
+                                 durable: true,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+
+                    //tells the queue not to dispatch more than 1 job.  This means the distribution of jobs is fair.  Without this
+                    //it will dispatch the nth job to the nth worker without regaurd to how long to job will take.  One thing to note
+                    //is that this could cause the queue to fill up and you will need a mitigation strategy such as spinning up new workers
+                    channel.BasicQos(0, 1, false);
+
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += (model, ea) =>
+                    {
+                        var body = ea.Body;
+                        var message = Encoding.UTF8.GetString(body);
+                        Console.WriteLine(" [x] Received {0}", message);
+
+                        int dots = message.Split('.').Length - 1;
+                        Thread.Sleep(dots * 1000);
+
+                        Console.WriteLine("Done");
+
+                        //Ack the jobs manually... if an ack is not recieved and the worker died, it will retransmit the message
+                        channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+                    };
+                    channel.BasicConsume(queue: "hello",
+                                         autoAck: false,
+                                         consumer: consumer);
+
+                    Console.WriteLine(" Press [enter] to exit.");
+                    Console.ReadLine();
+
+                }
+            }
+        }
+    }
+}
---
+++ Send/Send.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace Send
+{
+    class Send
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Ready to send!");
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+
+                    //durable true will ensure we don't lose messages even if the RabitMQ server dies
+                    channel.QueueDeclare(queue: "task_queue",
+                                 durable: true,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+
+                    string message = GetMessage(args);
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    var props = channel.CreateBasicProperties();
+                    //ensure we don't lose messages even if the RabitMQ server dies; not 100% accurate
+                    props.Persistent = true;
+
+                    channel.BasicPublish(exchange: "",
+                                         routingKey: "hello",
+                                         basicProperties: props,
+                                         body: body);
+                    Console.WriteLine(" [x] Sent {0}", message);
+                }
+
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
+        }
+
+        private static string GetMessage(string[] args)
+        {
+            return ((args.Length > 0) ? string.Join(" ", args) : "Hello World!");
+        }
+    }
+}
+
